c0e8d4e2167a0d7ac6e87b7d42bab047aed7f534
3namenik <36602322+3namenik@users.noreply.github.com>
2023-01-23 14:30:34 +0300

    Update template.php

--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 <? if ($arResult['DETAIL_PICTURE']['WEBP_SRC']) {
 ?>
     <picture>
-        <source srcset="<?= $arResult['DETAIL_PICTURE']['WEBP_SRC'] ?>" type="img/webp" />
+        <source srcset="<?= $arResult['DETAIL_PICTURE']['WEBP_SRC'] ?>" type="image/webp" />
         <source srcset="<?= $arResult['DETAIL_PICTURE']['SRC'] ?>" type="<?= $arResult['DETAIL_PICTURE']['CONTENT_TYPE'] ?>" />
         <img src="<?= $arResult['DETAIL_PICTURE']['SRC'] ?>" width="<?= $arResult['DETAIL_PICTURE']['WIDTH'] ?>" height="<?= $arResult['DETAIL_PICTURE']['HEIGHT'] ?>"  alt="<?= $arResult['DETAIL_PICTURE']['ALT'] ?>" loading="lazy" />
     </picture>
